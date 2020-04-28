5d61e909b3af91ef507281f67411536bf228caba
AnamariaIuga <anamaria.iuga01@gmail.com>
2020-04-28 16:44:49 +0000

    hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import { Container } from 'react-bootstrap';
+import {
+    ListGroup, 
+    ListGroupItem, 
+    Button,
+    Form,
+    Input,
+    InputGroup
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const initialTodoList = [
+    "Learn React Hooks",
+    "Code, Code, Code", 
+    "Fix all the bugs",
+    "Take a break",
+    "Code, Code, Code"
+];
+
+function TodoApp (){
+    const {
+        todo,
+        todoList,
+        handleInputChange,
+        handleSubmit, 
+        handleRemoveClick
+    } = useTodoState();
+    
+    return (
+        <Container>
+            <section> 
+                <h1>DAILY TODO LIST</h1>
+                <ListGroup>
+                    {todoList.map((item, i) => {
+                        return (
+                            <ListGroupItem key={i}>
+                            {item}
+                            <Button close onClick={() => handleRemoveClick(i)} />
+                            </ListGroupItem>
+                            );
+                    })}
+                </ListGroup>
+                <Form onSubmit = {handleSubmit}>
+                    <InputGroup>
+                        <Input value={todo} onChange={handleInputChange} />
+                        <Button>Add</Button>
+                    </InputGroup>
+                </Form>
+            </section>
+        </Container>
+    );
+ }
+
+ function useTodoState() {
+     const [todo, setTodo] = useState("");
+     const [todoList, setTodoList] = useState(initialTodoList); 
+ 
+    function handleInputChange(e) {
+        setTodo(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setTodoList([...todoList, todo]);
+        setTodo("");
+    }
+
+    function handleRemoveClick(todoIndex) {
+        const newTodoList = todoList.filter((_, i) => i !==todoIndex);
+        setTodoList(newTodoList);
+    }
+
+    return {
+        todo,
+        todoList,
+        handleInputChange,
+        handleSubmit,
+        handleRemoveClick
+    };
 }
 
-export default App;
+export default TodoApp;
